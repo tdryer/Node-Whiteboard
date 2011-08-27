ca37b792c23980f53d7c53ce9e474e9911037e68
David Yan <davidplusplus@gmail.com>
2011-08-26 18:20:04 -0700

    wrap js in anon function to run after page loads and rid global namespace polution

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-window.addEventListener('load', function () {
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
-  var pen_down = false;
-  canvas.addEventListener('mousemove', on_mousemove, false);
-  
-  function on_mousemove (ev) {
-    if (!pen_down) {
-      context.beginPath();
-      context.moveTo(ev.offsetX, ev.offsetY);
-      pen_down = true;
-    } else {
-      context.lineTo(ev.offsetX, ev.offsetY);
-      context.stroke();
+$(function(){
+  $('#clear-all').click(function(ev){
+    ev.preventDefault();
+    console.log('clear-all clicked');
+  });
+  window.addEventListener('load', function () {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var pen_down = false;
+    canvas.addEventListener('mousemove', on_mousemove, false);
+    
+    function on_mousemove (ev) {
+      if (!pen_down) {
+        context.beginPath();
+        context.moveTo(ev.offsetX, ev.offsetY);
+        pen_down = true;
+      } else {
+        context.lineTo(ev.offsetX, ev.offsetY);
+        context.stroke();
+      }
     }
-  }
-  
+  });
 });
