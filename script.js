b1455cf6d40b7d1660aa3923fdc4a9792f1f2f0e
David Yan <davidplusplus@gmail.com>
2011-08-26 18:56:41 -0700

    attempt at change color for the stroke

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var procede = function(name) {
       name: name
     }, function(data) {
       color = data;
+      console.log(color);
     }
   );
   $('#clear-all').click(function(ev){
@@ -13,6 +14,7 @@ var procede = function(name) {
   });
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
+  context.fillStyle = color;
   var pen_down = false;
   canvas.addEventListener('mousemove', on_mousemove, false);
   function on_mousemove (ev) {
