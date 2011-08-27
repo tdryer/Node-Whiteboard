cf36b640614585ac48bf914926cf548bf8e8b5ff
David Yan <davidplusplus@gmail.com>
2011-08-26 18:17:42 -0700

    properly send mimetypes for js and css

--- script.js
+++ script.js
@@ -16,4 +16,3 @@ window.addEventListener('load', function () {
   }
   
 });
-
--- server.js
+++ server.js
@@ -2,9 +2,12 @@
 var debug = process.argv[3] ? process.argv[3] : false;
 var port = process.argv[2] ? process.argv[2] : 80;
 
+var users = [];
+
 // modules
 var http = require('http')
   , url = require('url')
+  , path = require('path')
   , fs = require('fs');
   //, nko = require('nko')('NNlLWzf6EhahtxjJ');
 
@@ -18,9 +21,27 @@ var app = http.createServer(function (req, res) {
         res.end(data);
       });
     break;
+    case '/join':
+      fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      });
+    break;
     default:
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('Hello, World');
+      var file = path.join(process.cwd(), uri);
+      path.exists(file, function(exists) {
+        if (exists) {
+          var extension = file.lastIndexOf('.') < 0 ? '' : file.substring(file.lastIndexOf('.'));
+          fs.readFile(file, function(err, data) {
+            if (extension === '.css') {
+              res.writeHead(200, {'Content-Type': 'text/css'});
+            } else if (extension === '.js') {
+              res.writeHead(200, {'Content-Type': 'application/javascript'});
+            }
+            res.end(data);
+          });
+        }
+      });
     break;
   }
 
