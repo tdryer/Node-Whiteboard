d6aee0560087fd19bd50e20d7b01544491dbfd00
David Yan <davidplusplus@gmail.com>
2011-08-26 21:06:42 -0700

    file cleanup

--- server.js
+++ server.js
@@ -1,59 +1,57 @@
-// boss
 process.chdir(__dirname);
-
-// arguments
-var debug = process.argv[3] ? process.argv[3] : false;
-var port = process.argv[2] ? process.argv[2] : 80;
-
-var users = [], rooms = [];
-
-function genColor() {
-  return 'green';
-}
-
-// modules
-var http = require('http')
-  , url = require('url')
-  , path = require('path')
-  , fs = require('fs');
-  //, nko = require('nko')('NNlLWzf6EhahtxjJ');
+var lib = require('./helpers'),
+    http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
+    nko = 'hi'; // require('nko')('NNlLWzf6EhahtxjJ');
+var debug = process.argv[3] ? true : false,
+    port = process.argv[2] ? process.argv[2] : 80,
+    users = [],
+    rooms = [];
 
 var app = http.createServer(function (req, res) {
   var uri = url.parse(req.url).pathname;
-  
   switch (uri) {
+
     case '/':
       fs.readFile('index.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
       });
     break;
+
     case '/join':
       var name = url.parse(req.url).query.toString().replace('name=', '');
-      users[name] = {name: name, color: genColor()};
+      users[name] = {
+        name: name,
+        color: lib.genColor()
+      };
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end(users[name].color);
-      console.log(users);
+      debug && console.log(users);
     break;
+
     case '/draw':
       var drawer = url.parse(req.url).query.toString().replace('name=', '');
     break;
+
     case '/users':
-      var i;
+      var i, cur_users = [];
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      var cur_users = [];
       for (i in users) {
         cur_users.push(users[i].name);
       }
       res.end(JSON.stringify(cur_users));
     break;
+
     default:
-      var file = path.join(process.cwd(), uri);
+      var file = path.join(process.cwd(), uri), extension;
       path.exists(file, function(exists) {
         if (exists) {
-          var extension = file.lastIndexOf('.') < 0 ? '' : file.substring(file.lastIndexOf('.'));
+          extension = file.lastIndexOf('.') < 0 ? '' : file.substring(file.lastIndexOf('.'));
           fs.readFile(file, function(err, data) {
-            if (extension === '.css') {
+            if ( extension === '.css' ) {
               res.writeHead(200, {'Content-Type': 'text/css'});
             } else if (extension === '.js') {
               res.writeHead(200, {'Content-Type': 'application/javascript'});
@@ -64,17 +62,16 @@ var app = http.createServer(function (req, res) {
       });
     break;
   }
-
 });
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port + '.');
-  // if run as root, downgrade to the owner of this file
   try {
+    // if run as root, downgrade to the owner of this file
     if (process.getuid() === 0) {
       fs.stat(__filename, function(err, stats) {
         if (err) {
-          return console.log(err);
+          console.log(err);
         }
         process.setuid(stats.uid);
       });
