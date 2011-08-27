e642c25f8f130bed3cb277d9df5d2619cacee6d4
Michael Tom-Wing <mtomwing@gmail.com>
2011-08-26 19:51:56 -0700

    Added deployment script. Usage: ./deploy linode

--- server.js
+++ server.js
@@ -56,6 +56,16 @@ var app = http.createServer(function (req, res) {
 
 });
 
+//app.listen(port, function() {
+//  console.log('Server listening on port ' + port + '.');
+//});
+
 app.listen(port, function() {
-  console.log('Server listening on port ' + port + '.');
+  console.log('Ready');
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0)
+  require('fs').stat(__filename, function(err, stats) {
+    if (err) return console.log(err)
+    process.setuid(stats.uid);
+  });
 });
