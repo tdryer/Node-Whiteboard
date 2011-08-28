4db0c352075c918d25d34ac01f338871d9266887
Michael Tom-Wing <mtomwing@gmail.com>
2011-08-27 23:18:37 -0700

    Minor whiteboard.js improvements.

--- test.whiteboard.js
+++ test.whiteboard.js
@@ -2,3 +2,5 @@ var Whiteboard = require('./whiteboard').Whiteboard;
 
 var x = new Whiteboard('mofomofo');
 x.load();
+x.add_line(1, 2, 3, 4);
+x.print_board();
--- whiteboard.js
+++ whiteboard.js
@@ -1,7 +1,32 @@
-module.exports.Whiteboard = function(name) {
-  this.name = name;
-  this.board = [];
-  this.load = function() {
-    console.log('STUB: load whiteboard=' + this.name + ' from database');
+String.prototype.format = function() {
+  var formatted = this;
+  for(arg in arguments) {
+    formatted = formatted.replace("{" + arg + "}", arguments[arg]);
   }
+  return formatted;
+};
+
+function Line(x1, y1, x2, y2) {
+  this.src = [x1, y1]; // (x, y)
+  this.dest = [x2, y2]; // (x, y)
+  this.pretty = function() {
+    return 'src({0},{1}) ==> dest({2},{3})'.format(this.src[0], this.src[1], this.dest[0], this.dest[1]);
+  };
+}
+
+module.exports.Whiteboard = function(name) {
+  this.name = name; // unique
+  this.board = []; // list of line coordinates src(x, y), dest(x, y)
+  this.room = []; // list of user references
+  this.load = function() { // load from database/cache
+    console.log('STUB: load whiteboard=' + this.name + ' from database');
+  };
+  this.add_line = function(x1, y1, x2, y2) {
+    this.board.push(new Line(x1, y1, x2, y2));
+  };
+  this.print_board = function() {
+    for (line in this.board) {
+      console.log(line.pretty());
+    }
+  };
 }
